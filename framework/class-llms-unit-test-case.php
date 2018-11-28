8c303131f254fd6b06920045b1b339af5a91e981
Thomas Patrick Levy <thomasplevy@gmail.com>
2018-11-28 13:16:12 -0800

    add requestmock functions to unit test case

--- framework/class-llms-unit-test-case.php
+++ framework/class-llms-unit-test-case.php
@@ -17,5 +17,46 @@ class LLMS_Unit_Test_Case extends WP_UnitTestCase {
 
 	}
 
+	/**
+	 * Setup Get data to mock post and request data
+	 *
+	 * @param    array      $vars  mock get data
+	 * @return   void
+	 * @since    [version]
+	 * @version  [version]
+	 */
+	protected function mockGetRequest( $vars = array() ) {
+		$this->mockRequest( 'GET', $vars );
+	}
+
+	/**
+	 * Setup Post data to mock post and request data
+	 *
+	 * @param    array      $vars  mock post data
+	 * @return   void
+	 * @since    [version]
+	 * @version  [version]
+	 */
+	protected function mockPostRequest( $vars = array() ) {
+		$this->mockRequest( 'POST', $vars );
+	}
+
+	/**
+	 * Setup reuqest data to mock post/get and request data
+	 *
+	 * @param    array      $vars  mock request data
+	 * @return   void
+	 * @since    [version]
+	 * @version  [version]
+	 */
+	private function mockRequest( $method, $vars = array() ) {
+		putenv( 'REQUEST_METHOD=' . $method );
+		if ( 'POST' === $method ) {
+			$_POST = $vars;
+		} elseif ( 'GET' === $method ) {
+			$_GET = $vars;
+		}
+		$_REQUEST = $vars;
+	}
 
 }
