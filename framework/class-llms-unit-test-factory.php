374fcd844ed3fc3813c4d154b271585ea2da4807
Thomas Patrick Levy <thomasplevy@gmail.com>
2019-02-07 13:48:00 -0800

    add a membership factory

--- framework/class-llms-unit-test-factory.php
+++ framework/class-llms-unit-test-factory.php
@@ -19,6 +19,13 @@ class LLMS_Unit_Test_Factory extends WP_UnitTest_Factory {
 	public $instructor;
 
 	/**
+	 * LLMS_Unit_Test_Factory_For_Membership
+	 *
+	 * @var obj
+	 */
+	public $membership;
+
+	/**
 	 * LLMS_Unit_Test_Factory_For_Student
 	 *
 	 * @var obj
@@ -34,6 +41,7 @@ class LLMS_Unit_Test_Factory extends WP_UnitTest_Factory {
 
 		$this->course  = new LLMS_Unit_Test_Factory_For_Course( $this );
 		$this->instructor = new LLMS_Unit_Test_Factory_For_Instructor( $this );
+		$this->membership = new LLMS_Unit_Test_Factory_For_Membership( $this );
 		$this->student = new LLMS_Unit_Test_Factory_For_Student( $this );
 
 	}
