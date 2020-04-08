dc4975c053a96dc854e123759dc73a68caa30d06
Thomas Patrick Levy <thomasplevy@gmail.com>
2020-04-08 14:01:16 -0700

    remove duplicate trait declaration

--- framework/traits/trait-llms-unit-test-case-base.php
+++ framework/traits/trait-llms-unit-test-case-base.php
@@ -22,7 +22,6 @@ trait LLMS_Unit_Test_Case_Base {
 	use LLMS_Unit_Test_Assertions_Notices;
 	use LLMS_Unit_Test_Assertions_Assets;
 	use LLMS_Unit_Test_Assertions_Output;
-	use LLMS_Unit_Test_Assertions_String;
 	use LLMS_Unit_Test_Assertions_WP_Error;
 	use LLMS_Unit_Test_Mock_Requests;
 
