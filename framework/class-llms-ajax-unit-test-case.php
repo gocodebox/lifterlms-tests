39a86db3dc4feb9f326fa7e83cc96caa928c7981
Rocco Aliberti <raliberti84@gmail.com>
2023-10-23 11:02:38 +0200

    allow dynamic properties

--- framework/class-llms-ajax-unit-test-case.php
+++ framework/class-llms-ajax-unit-test-case.php
@@ -1,10 +1,18 @@
 <?php
 /**
- * LifterLMS AJAX Test Case
+ * LifterLMS AJAX Test Case.
  *
  * @since 1.5.0
- * @version 1.5.0
+ * @version [version]
  */
+
+/**
+ * LifterLMS AJAX Test Case class.
+ *
+ * @since 1.5.0
+ * @since [version] Allow dynamic properties.
+ */
+#[AllowDynamicProperties]
 class LLMS_Ajax_Unit_Test_Case extends WP_Ajax_UnitTestCase {
 
 	use LLMS_Unit_Test_Case_Base;
